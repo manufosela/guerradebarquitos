6d4436394dbcd63d057e56da025d822225def611
manufosela <mjfosela@gmail.com>
2015-01-15 09:21:37 +0100

    restructure files and new config files

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'Guerra de barquitos',
+  description: 'El clásico juego de la guerra de barquitos para jugar contra alguien',
+  version: '1.0.0'
+});
+
+App.icons({
+  'android_ldpi': 'resources/icons/icon-ldpi.png',
+  'android_mdpi': 'resources/icons/icon-mdpi.png',
+  'android_hdpi': 'resources/icons/icon-hdpi.png',
+  'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+});
